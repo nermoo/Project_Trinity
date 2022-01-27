ad683e0464285acaac88adaebbe7275d0ae3c732
nermoo <53093663+nermoo@users.noreply.github.com>
2022-01-27 11:11:32 +0530

    add followers list component

---
+++ client/src/components/followers.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Grid, Card, Avatar, Typography } from '@mui/material';
+import Scroll from 'react-scroll';
+
+
+const Followers=()=>{ //we can use props to pass the height to this component from dashboard
+
+    let innerHeight=window.innerHeight;
+    const Element=Scroll.Element
+    const followers=[{name:'aravinda nawarathna', image:'https://picsum.photos/200/300'},{name:'thejana lakshan', image:'https://picsum.photos/200/300'},{name:'will smith', image:'https://picsum.photos/200/300'},{name:'henry cavill', image:'https://picsum.photos/200/300'},{name:'json statham', image:'https://picsum.photos/200/300'},{name:'hugh jackman', image:'https://picsum.photos/200/300'},{name:'tom cruise', image:'https://picsum.photos/200/300'},{name:'superman', image:'https://picsum.photos/200/300'},{name:'matt damon', image:'https://picsum.photos/200/300'}]
+
+    return(
+        <Grid container>
+            <Grid item xs={2}></Grid>
+            <Grid item xs={8}>
+            <Element  className="element" id="containerElement" style={{
+          position: 'relative',
+          height:'280px',
+          overflow: 'scroll',
+          overflowX:'hidden',
+          scrollbarWidth:'none'
+          
+
+          
+        }}>
+            {
+                followers.map((follower)=>(
+                    <Card sx={{marginBottom:'10px'}} variant='outlined'>
+                    <Grid container>
+                        <Grid item xs={2}>
+                        <Avatar sx={{margin:'10px',marginLeft:'auto',marginRight:'auto'}} alt="Travis Howard" src={follower.image} />
+                        </Grid>
+                        <Grid sx={{marginTop:'auto',marginBottom:'auto'}} item xs={10}>
+                            <Typography >
+                            {follower.name}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                
+                
+                </Card>
+                ))
+            }
+        </Element>
+                
+            </Grid>
+            <Grid item xs={2}></Grid>
+        </Grid>
+    );
+}
+
+export default Followers;
