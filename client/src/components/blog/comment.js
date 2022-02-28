56fac4e6ff5a7437d5b199e7216c291965aaed53
nermoo <53093663+nermoo@users.noreply.github.com>
2022-02-28 13:14:48 +0530

    created comment component

---
+++ client/src/components/blog/comment.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Grid,Box } from '@mui/material';
+
+
+const Comment=()=>{
+
+    return(
+        <Grid container>
+            <Grid item xs={12}>
+
+            <Box>
+                Aravinda kolitha nawarathna
+            </Box>
+            </Grid>
+        </Grid>
+    );
+}
+
+
+export default Comment;
