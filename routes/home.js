1cdd9a1e88c47289d5d7fb032b2ed028c9b6f911
nermoo <53093663+nermoo@users.noreply.github.com>
2022-03-21 20:57:44 +0530

    finished quickaccess backend

--- routes/home.js
+++ routes/home.js
@@ -4,9 +4,7 @@ const posts=require('./../models/post');
 
 router.get('/', async (req,res)=>{
 
-    // const postList=await posts.aggregate([{$sample:{$size:4}}]);
-    // res.send(postList);
-    posts.find({},{authorid:0,post:0,title:0,day:0,month:0,tags:0,image:0,__v:0},function(err,docs){
+    posts.aggregate([{$sample:{size: 20}},{$project:{authorid:0,post:0,title:0,day:0,month:0,tags:0,image:0,__v:0}}],function(err,docs){
         if(err){
             console.log(err);
         }else{
@@ -39,37 +37,67 @@ router.post('/',(req,res)=>{
 //     res.send("hello from about");
 // })
 
-router.get('/quick',(req,res,next)=>{
-    // posts.aggregate([{$group:{
-    //     _id:'$authorid',
-    //     count:{$sum:1}
-    // }}],function(err,docs){
-    //     console.log('here are the' );
-    //     console.log(docs);
-    //     res.send(docs);
-    // }) 
-
-    posts.aggregate([
-        { "$addFields": { 
-        "tag": { "$split": [ "$tags", "," ] } 
+router.get('/quick',async (req,res,next)=>{
+    const authors=[];
+    const tags=[];
+    
+   await posts.aggregate([
+       {
+           $group:{
+            _id:'$authorid',
+            count:{$sum:1}
         }},
-        { $unwind : "$tag" },
-        {$group:{
+        { 
+            $limit : 5 
+        },
+        {
+            $sort:{count:-1}
+        }
+    ],function(err,docs){
+        console.log(docs);
+        docs.map(doc=>{
+            authors.push(doc)
+        })
+
+    }) 
+
+    await posts.aggregate([
+        { 
+            $addFields: { 
+            "tag": { "$split": [ "$tags", "," ] } 
+        }},
+        { 
+            $unwind : "$tag" 
+        },
+        {
+            $group:{
                 _id:'$tag',
                 count:{$sum:1}
          }},
+         { 
+             $limit : 5 
+         },
          {
              $sort:{count:-1}
          }
-],function(err,docs){
-        console.log('here are the' );
-        console.log(docs);
-        res.send(docs);
-    });
+    ],function(err,docs){
+
+        if(err){
+            console.log(err);
+        }else{
+            console.log(docs);
+            docs.map(doc=>{
+                tags.push(doc)
+            })
+            res.send({tags:tags,authors:authors});
+        }
+            
+            
+        });
+
     
+
     
-    
-    // methnta unwind use krla blnda one loop krnda puluwanda kyla
 })
 
 
